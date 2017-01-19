e6b45f52d2f2197211b5784029c7a7d241f8fb66
kmarshall <Kevin Marshall>
2017-01-19 14:21:20 -0500

    Changed normalization to max(row) for each row. Now saves a copy of normed matrix before we remove extra entries for bootstrapping. Specified additional parameters to solver.

--- solve.py
+++ solve.py
@@ -9,12 +9,16 @@ unnorm= data[1:,1:] #Last row is normalization ... need to change norm.. somehow
 incompl = []
 
 for el in unnorm:
-	incompl.append([float(x)/float(el[-1]) if float(el[-1]) !=0 else 0 for x in el[:-1]])
+ 	#incompl.append([float(x)/float(el[-1]) if float(el[-1]) !=0 else 0 for x in el[:-1]]) #Does norm even matter??
+	#incompl.append([float(x) for x in el[:-1]])
+	maxn = max([float(x) for x in el[:-1]])
+	incompl.append([float(x)/maxn if float(maxn) !=0 else 0 for x in el[:-1]])
 
 incompl= np.asarray(incompl)
 incompl[incompl == 0] = np.nan
+np.savetxt("normed.csv", incompl, delimiter=",") #Save the formatted matrix for comparison after we remove extra entries for bootstrapping
+incompl[3,6]=np.nan
 mask = np.isnan(incompl)
-solver = NuclearNormMinimization(require_symmetric_solution=False)
+solver = NuclearNormMinimization(require_symmetric_solution=False, min_value= 0, max_value=1,fast_but_approximate=False)
 solved = solver.complete(incompl)
-
 np.savetxt("solved.csv", solved, delimiter=",")
