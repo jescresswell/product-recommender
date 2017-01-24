d1022a3f8536a8dd4c29361bba34010a0b5a113b
kmarshall <Kevin Marshall>
2017-01-24 14:32:08 -0500

    Added comments, added re-constructed data csv files to repository

--- solve.py
+++ solve.py
@@ -1,8 +1,6 @@
 import numpy as np
 from numpy.linalg import matrix_rank
-import csv
 from fancyimpute import NuclearNormMinimization, KNN
-from common import reconstruction_error
 
 data = np.genfromtxt('query_result.csv', delimiter=',', dtype=None) #Read entire CSV into array
 
@@ -13,21 +11,21 @@ for el in unnorm:
  	#incompl.append([float(x)/float(el[-1]) if float(el[-1]) !=0 else 0 for x in el[:-1]]) #Does norm even matter??
 	#incompl.append([float(x) for x in el[:-1]])
 	maxn = max([float(x) for x in el[:-1]])
-	incompl.append([round(float(x)/maxn*5)/5 if float(maxn) !=0 else 0 for x in el[:-1]])
+	incompl.append([round(float(x)/maxn*5)/5 if float(maxn) !=0 else 0 for x in el[:-1]]) #Round to nearest 0.2n
 
 incompl= np.asarray(incompl)
 incompl[incompl == 0] = np.nan
 np.savetxt("normed.csv", incompl, delimiter=",") #Save the formatted matrix for comparison after we remove extra entries for bootstrapping
 
 mask = np.isnan(incompl)
-#solver = NuclearNormMinimization(require_symmetric_solution=False, min_value= 0, max_value=1,fast_but_approximate=False)
-solver = KNN(k=3, min_value=0, max_value=1)
+solver = NuclearNormMinimization(require_symmetric_solution=False, min_value= 0, max_value=1,fast_but_approximate=False)
+#solver = KNN(k=3, min_value=0, max_value=1) #This seems to have problems with overlaps, see errors?
 solved = solver.complete(incompl)
-#solved = [np.round(el*5)/5 for el in solved]
+#solved = [np.round(el*5)/5 for el in solved] #This just made things worse, i.e., higher rank solution
 np.savetxt("solved.csv", solved, delimiter=",")
-u,s,v = np.linalg.svd(solved)
-
-print(s)
+np.savetxt("mask.csv", mask, delimiter=",") #Ones where we reconstructed the data, zeros where we already had enteries
+#u,s,v = np.linalg.svd(solved)
+#print(s)
 
 print("Nuclear norm minimization perfectly recovers most "
 	  "low-rank nxn matrices of rank r, if O(n^1.2 r log n) entries sampled uniformly at random are observed")
