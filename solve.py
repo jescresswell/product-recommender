a06803330431915c94137632623783fc58a25578
kmarshall <Kevin Marshall>
2017-01-24 14:12:45 -0500

    Tried coarse-graining (rounding) the initial matrix and also tried KNN solver instead

--- solve.py
+++ solve.py
@@ -1,7 +1,7 @@
 import numpy as np
 from numpy.linalg import matrix_rank
 import csv
-from fancyimpute import NuclearNormMinimization
+from fancyimpute import NuclearNormMinimization, KNN
 from common import reconstruction_error
 
 data = np.genfromtxt('query_result.csv', delimiter=',', dtype=None) #Read entire CSV into array
@@ -13,16 +13,21 @@ for el in unnorm:
  	#incompl.append([float(x)/float(el[-1]) if float(el[-1]) !=0 else 0 for x in el[:-1]]) #Does norm even matter??
 	#incompl.append([float(x) for x in el[:-1]])
 	maxn = max([float(x) for x in el[:-1]])
-	incompl.append([float(x)/maxn if float(maxn) !=0 else 0 for x in el[:-1]])
+	incompl.append([round(float(x)/maxn*5)/5 if float(maxn) !=0 else 0 for x in el[:-1]])
 
 incompl= np.asarray(incompl)
 incompl[incompl == 0] = np.nan
 np.savetxt("normed.csv", incompl, delimiter=",") #Save the formatted matrix for comparison after we remove extra entries for bootstrapping
 
 mask = np.isnan(incompl)
-solver = NuclearNormMinimization(require_symmetric_solution=False, min_value= 0, max_value=1,fast_but_approximate=False)
+#solver = NuclearNormMinimization(require_symmetric_solution=False, min_value= 0, max_value=1,fast_but_approximate=False)
+solver = KNN(k=3, min_value=0, max_value=1)
 solved = solver.complete(incompl)
+#solved = [np.round(el*5)/5 for el in solved]
 np.savetxt("solved.csv", solved, delimiter=",")
+u,s,v = np.linalg.svd(solved)
+
+print(s)
 
 print("Nuclear norm minimization perfectly recovers most "
 	  "low-rank nxn matrices of rank r, if O(n^1.2 r log n) entries sampled uniformly at random are observed")
