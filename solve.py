c1fa936d50140753291e83a7d58ee54f87d6afaa
jescresswell <jcresswe@physics.utoronto.ca>
2017-01-26 19:24:47 -0500

    Removed rows with no information

--- solve.py
+++ solve.py
@@ -7,13 +7,15 @@ unnorm= data[1:,1:] #Last row is normalization ... need to change norm.. somehow
 incompl = []
 
 for el in unnorm:
- 	#incompl.append([float(x)/float(el[-1]) if float(el[-1]) !=0 else 0 for x in el[:-1]]) #Does norm even matter??
-	#incompl.append([float(x) for x in el[:-1]])
-	maxn = max([float(x) for x in el[:-1]])
-	#incompl.append([float(x)/maxn if float(maxn) !=0 else 0 for x in el[:-1]])
-	incompl.append([round(float(x)/maxn*5)/5 if float(maxn) !=0 else 0 for x in el[:-1]]) #Round to nearest 0.2n
+    if float(el[-1]) != float(0):# remove rows which have no information
+        #incompl.append([float(x)/float(el[-1]) if float(el[-1]) !=0 else 0 for x in el[:-1]]) #Does norm even matter??
+        #incompl.append([float(x) for x in el[:-1]])
+        maxn = max([float(x) for x in el[:-1]])
+        #incompl.append([float(x)/maxn if float(maxn) !=0 else 0 for x in el[:-1]])
+        incompl.append([round(float(x)/maxn*5)/5 if float(maxn) !=0 else 0 for x in el[:-1]]) #Round to nearest 0.2n
 
 incompl= np.asarray(incompl)
+data_rank=np.linalg.matrix_rank(incompl)
 incompl[incompl == 0] = np.nan
 np.savetxt("normed.csv", incompl, delimiter=",") #Save the formatted matrix for comparison after we remove extra entries for bootstrapping
 
@@ -32,8 +34,9 @@ np.savetxt("mask.csv", mask, delimiter=",") #ones where we reconstructed the dat
 
 # print("Nuclear norm minimization perfectly recovers most "
 # 	  "low-rank nxn matrices of rank r, if O(n^1.2 r log n) entries sampled uniformly at random are observed")
-# print ("n^1.2 r log n = " + str(np.power(len(incompl),1.2)*np.linalg.matrix_rank(solved)*np.log(len(incompl))))
+# print ("n^1.2 r log n = " + str(np.power(len(incompl),1.2)*data_rank*np.log(len(incompl))))
 # print ("Total elements in matrix: " + str(incompl.shape[0]*incompl.shape[1]))
+# print ("Our data matrix has " + str(incompl.shape[0]) + " rows (customers) and "+str(incompl.shape[1]))+" columns (products)
 # print ("Elements we knew in matrix: "+ str(np.sum(np.logical_not(mask))))
 
 #Bootstrapping
